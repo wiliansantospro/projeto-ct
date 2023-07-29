1d19f696e66860fa2e3d946f14a2de4b69552c57
Lucas Bizachi <bizachilucas@gmail.com>
2023-07-29 16:41:06 -0300

    add pasta app

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
 ]
