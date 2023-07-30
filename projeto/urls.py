3f2807402a94c907664ff8e498dd5c7d07164b9f
Lucas Bizachi <bizachilucas@gmail.com>
2023-07-29 21:33:37 -0300

    add global e app

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse('Home')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', include('appct.urls'))
 ]
