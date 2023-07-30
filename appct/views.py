180a635507aeec1ee290e67ca57e610cde81fd9c
Lucas Bizachi <bizachilucas@gmail.com>
2023-07-30 18:49:23 -0300

    add pages static

--- appct/views.py
+++ appct/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'appct/home.html')
+    return render(request, 'appct/pages/home.html')
